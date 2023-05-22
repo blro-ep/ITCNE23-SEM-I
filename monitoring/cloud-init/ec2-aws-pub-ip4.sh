6ba086231f77f846add233d02b989ce7a4971c71
Roger <roeschu.bluem@gmail.com>
2023-05-22 22:42:03 +0200

    update ec2 scripts

---
+++ monitoring/cloud-init/ec2-aws-pub-ip4.sh
@@ -0,0 +1,2 @@
+TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"` \
+&& curl -H "X-aws-ec2-metadata-token: $TOKEN" -v http://169.254.169.254/latest/meta-data/public-ipv4
