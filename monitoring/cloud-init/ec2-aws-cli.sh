4cd9d43438ceb6ed72936c4d610edada73cc7235
Roger <roeschu.bluem@gmail.com>
2023-06-01 23:31:38 +0200

    add pub key cloud init, remove key aws-cli

--- monitoring/cloud-init/ec2-aws-cli.sh
+++ monitoring/cloud-init/ec2-aws-cli.sh
@@ -6,11 +6,8 @@
 AWS_IMAGE="ami-04dbb668869bef978"
 AWS_REGION="eu-central-2"
 INSTANCE_TYPE="t3.micro"
-KEY_PAIR_NAME="id_aws"
-SECURITY_GROUP_ID="sg-06007a2972577f41e"
-SUBNET_ID="subnet-0b146988807ff7349"
-
-
+SECURITY_GROUP_ID="sg-08247dc225eecc759"
+SUBNET_ID="subnet-0ea0164c607c3531c"
 
 # create ec2 instace
 aws ec2 run-instances \
@@ -18,7 +15,6 @@ aws ec2 run-instances \
     --image-id $AWS_IMAGE \
     --count 1 \
     --instance-type $INSTANCE_TYPE \
-    --key-name $KEY_PAIR_NAME \
     --security-group-ids $SECURITY_GROUP_ID \
     --subnet-id $SUBNET_ID \
     --user-data file://cloud-config-podman.yaml
