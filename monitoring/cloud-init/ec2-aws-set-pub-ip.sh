cf98359fcbba5425d3820da3a97536f2fad060a7
Roger <roeschu.bluem@gmail.com>
2023-06-03 12:52:18 +0200

    mod ip variable name

--- monitoring/cloud-init/ec2-aws-set-pub-ip.sh
+++ monitoring/cloud-init/ec2-aws-set-pub-ip.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # get aws local IPv4
-AWS-LOCAL-IPV4=$(cloud-init query -a | jq -r '."ds"."meta-data"."local-ipv4"')
+AWS_LOCAL_IPV4=$(cloud-init query -a | jq -r '."ds"."meta-data"."local-ipv4"')
 
 # set local ip for prometheus
-sudo sed -i "s/xxx.xxx.xxx.xxx/$AWS-LOCAL-IPV4/g" /opt/ITCNE23-SEM-I/monitoring/prometheus/prometheus.yml
+sudo sed -i "s/xxx.xxx.xxx.xxx/$AWS_LOCAL_IPV4/g" /opt/ITCNE23-SEM-I/monitoring/prometheus/prometheus.yml
