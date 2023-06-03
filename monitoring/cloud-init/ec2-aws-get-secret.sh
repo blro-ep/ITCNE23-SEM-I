259214f098920898167020d569edfc5cdaf86b7f
Roger <roeschu.bluem@gmail.com>
2023-06-03 23:07:02 +0200

    unset secret variable

--- monitoring/cloud-init/ec2-aws-get-secret.sh
+++ monitoring/cloud-init/ec2-aws-get-secret.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # get secret for gmail 
-AWS_SECRET_GMAIL=$(aws secretsmanager get-secret-value --region eu-central-2 --secret-id test | jq -r '.SecretString | fromjson | .gmail'
+AWS_SECRET_GMAIL=$(aws secretsmanager get-secret-value --region eu-central-2 --secret-id test | jq -r '.SecretString | fromjson | .gmail')
 
 # set secret for alertmanager
 sudo sed -i "s/xxxxxxxxxxx/$AWS_SECRET_GMAIL/g" /opt/ITCNE23-SEM-I/monitoring/alertmanager/alertmanager.yml
+
+# delete secret
+unset AWS_SECRET_GMAIL
