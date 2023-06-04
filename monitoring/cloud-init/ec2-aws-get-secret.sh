7e9e39790d9eaf2b04b083fdb49b7a2a9839188b
Roger <roeschu.bluem@gmail.com>
2023-06-04 19:50:33 +0200

    add mailaddress to Secret

--- monitoring/cloud-init/ec2-aws-get-secret.sh
+++ monitoring/cloud-init/ec2-aws-get-secret.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 # get secret for gmail 
-AWS_SECRET_GMAIL=$(aws secretsmanager get-secret-value --region eu-central-2 --secret-id prometheus_alertmanager | jq -r '.SecretString | fromjson | .email')
+AWS_SECRET_MAILADDRESS=$($(aws secretsmanager get-secret-value --region eu-central-2 --secret-id prometheus_alertmanager | jq -r '.SecretString | fromjson | .email'))
+AWS_SECRET_GMAILPW=$(aws secretsmanager get-secret-value --region eu-central-2 --secret-id prometheus_alertmanager | jq -r '.SecretString | fromjson | .password')
 
 # set secret for alertmanager
-sudo sed -i "s/xxxxxxxxxxx/$AWS_SECRET_GMAIL/g" /opt/ITCNE23-SEM-I/monitoring/alertmanager/alertmanager.yml
-sudo sed -i "s/xxx@gmail.com/regormulb@gmail.com/g" /opt/ITCNE23-SEM-I/monitoring/alertmanager/alertmanager.yml
+sudo sed -i "s/xxx@gmail.com/$AWS_SECRET_MAILADDRESS/g" /opt/ITCNE23-SEM-I/monitoring/alertmanager/alertmanager.yml
+sudo sed -i "s/xxxxxxxxxxx/$AWS_SECRET_GMAILPW/g" /opt/ITCNE23-SEM-I/monitoring/alertmanager/alertmanager.yml
+
 
 
 # delete secret
-unset AWS_SECRET_GMAIL
+unset AWS_SECRET_MAILADDRESS
+unset AWS_SECRET_GMAILPW
