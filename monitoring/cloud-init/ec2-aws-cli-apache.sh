8571b6a124d4545ffa2865532a6235a411db8bc7
Roger <roeschu.bluem@gmail.com>
2023-06-04 22:21:54 +0200

    add apache user-data / apache bash script

---
+++ monitoring/cloud-init/ec2-aws-cli-apache.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# script for testing aws
+
+# variables
+AWS_IMAGE="ami-04dbb668869bef978"
+AWS_REGION="eu-central-2"
+INSTANCE_TYPE="t3.micro"
+SECURITY_GROUP_ID="sg-08247dc225eecc759"
+SUBNET_ID="subnet-0ea0164c607c3531c"
+
+# create ec2 instace
+aws ec2 run-instances \
+    --region $AWS_REGION \
+    --image-id $AWS_IMAGE \
+    --count 1 \
+    --instance-type $INSTANCE_TYPE \
+    --security-group-ids $SECURITY_GROUP_ID \
+    --subnet-id $SUBNET_ID \
+    --iam-instance-profile Name="SecretsManager" \
+    --user-data file://cloud-init-apache.yaml
