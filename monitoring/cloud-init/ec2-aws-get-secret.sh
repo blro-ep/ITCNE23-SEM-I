b7957e7026af2b0b5dbe245ec9b8e7a8546472de
Roger <roeschu.bluem@gmail.com>
2023-06-04 17:08:29 +0200

    testing aws secret manager

--- monitoring/cloud-init/ec2-aws-get-secret.sh
+++ monitoring/cloud-init/ec2-aws-get-secret.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 # get secret for gmail 
-AWS_SECRET_GMAIL=$(aws secretsmanager get-secret-value --region eu-central-2 --secret-id test | jq -r '.SecretString | fromjson | .gmail')
+AWS_SECRET_GMAIL=$(aws secretsmanager get-secret-value --region eu-central-2 --secret-id test | jq -r '.SecretString | fromjson | .regormulb@gmail.com')
 
 # set secret for alertmanager
 sudo sed -i "s/xxxxxxxxxxx/$AWS_SECRET_GMAIL/g" /opt/ITCNE23-SEM-I/monitoring/alertmanager/alertmanager.yml
+sudo sed -i "s/xxx@gmail.com/regormulb@gmail.com/g" /opt/ITCNE23-SEM-I/monitoring/alertmanager/alertmanager.yml
+
 
 # delete secret
 unset AWS_SECRET_GMAIL
